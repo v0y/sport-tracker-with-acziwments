c8df6d4ad10f0115cb92381c4bd278c3e7872dc7
Rafał Mirończyk <voyageur.pl@gmail.com>
2013-11-18 21:21:12 +0100

    #33: add test for Health.test_get_datas

--- app/health/tests.py
+++ app/health/tests.py
@@ -18,3 +18,39 @@ class TestHealth(UserProfileTestCase):
         self.assertEqual(u3_first_date, None)
         self.assertEqual(u4_first_date, '2013 01 02')
 
+    def test_get_datas(self):
+        u1_datas = Health.get_datas(self.u1, 'year', '2013')
+        u2_datas = Health.get_datas(self.u2, 'week', '2013-01-02')
+        u3_datas = Health.get_datas(self.u3, 'month', '2013-01')
+        u4_datas = Health.get_datas(self.u4, 'month', '2013-01')
+
+        u1_expected_datas = {
+            'weight': [{'x': '2013-01-01', 'y': 100}],
+            'fat': [{'x': '2013-01-01', 'y': None}],
+            'water': [{'x': '2013-01-01', 'y': None}]
+        }
+        u2_expected_datas = {
+            'weight': [
+                {'x': '2013-01-02', 'y': None},
+                {'x': '2013-01-03', 'y': None}
+            ],
+            'fat': [
+                {'x': '2013-01-02', 'y': 43},
+                {'x': '2013-01-03', 'y': None}
+            ],
+            'water': [
+                {'x': '2013-01-02', 'y': 1.9},
+                {'x': '2013-01-03', 'y': 1}
+            ]
+        }
+        u3_expected_datas = {'weight': [], 'fat': [], 'water': []}
+        u4_expected_datas = {
+            'weight': [{'x': '2013-01-02', 'y': 98}],
+            'fat': [{'x': '2013-01-02', 'y': 69}],
+            'water': [{'x': '2013-01-02', 'y': 13}]
+        }
+
+        self.assertEqual(u1_datas, u1_expected_datas)
+        self.assertEqual(u2_datas, u2_expected_datas)
+        self.assertEqual(u3_datas, u3_expected_datas)
+        self.assertEqual(u4_datas, u4_expected_datas)
