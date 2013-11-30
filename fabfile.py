37bd19a03a8b54a2dab201a9a414c55ba251807f
Rafał Mirończyk <voyageur.pl@gmail.com>
2013-11-30 18:44:05 +0100

    add fab tasks for install requirements

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,38 @@
 # encoding: utf-8
 
+from os.path import dirname, join, realpath
+
+from fabric.context_managers import lcd
 from fabric.operations import local
 from fabric.tasks import execute
 
 
-def clear_database():
+_current_dir = dirname(realpath(__file__))
+
+
+def drop_database():
     """
     Drop and create database.
     """
-    sql = 'DROP DATABASE stwa; ' \
-          'CREATE DATABASE stwa ' \
-          'CHARACTER SET utf8 COLLATE utf8_general_ci;'
-    local("echo '%s' | python ./manage.py dbshell" % sql)
+    local("echo 'DROP DATABASE stwa;' | python ./manage.py dbshell")
 
 
 def create_database():
     """
     Create database with devdata, create superuser.
     """
+    # create database
+    sql = 'CREATE DATABASE stwa ' \
+          'CHARACTER SET utf8 COLLATE utf8_general_ci;'
+    local("echo '%s' | python ./manage.py dbshell" % sql)
+
+    # sync
     local("./manage.py syncdb --noinput -v0")
     local("./manage.py migrate -v0 --no-initial-data")
+
     execute(create_superuser)
+
+    # load data
     local("./manage.py loaddata devdata initial_data -v0")
 
 
@@ -33,9 +45,39 @@ def create_superuser(username='admin', password='a'):
     local("./manage.py set_password %s %s" % (username, password))
 
 
+def install_requirements():
+    """
+    Install pip and bower requirements.
+    """
+    local("pip install -r requirements.txt")
+    local("./manage.py bower_install")
+
+
+def lets_rock():
+    """
+    Install all required packages and create database.
+    """
+    # Node.js
+    local("curl http://nodejs.org/dist/node-latest.tar.gz | tar xvz")
+    with lcd(join(_current_dir, "node-v*")):
+        local("./configure --prefix=$VIRTUAL_ENV")
+        local("make install")
+
+    # npm
+    local("curl https://npmjs.org/install.sh | sh")
+
+    # npm requirements
+    local("cat npm-requirements.txt | xargs -I % npm install %")
+
+    # Install pip and bower requirements
+    execute(install_requirements)
+
+    execute(create_database)
+
+
 def recreate_database():
     """
     Drop and create database, run syncdb and migrations, create superuser.
     """
-    execute(clear_database)
+    execute(drop_database)
     execute(create_database)
