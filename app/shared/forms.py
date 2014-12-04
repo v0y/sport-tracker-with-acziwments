6cdb86ed8b9a49d84dd9fe3088e8576cccd9db85
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-12-04 23:47:09 +0100

    fix select date with none widget

--- app/shared/forms.py
+++ app/shared/forms.py
@@ -6,7 +6,7 @@ from django.forms import extras
 class SelectDateWidgetWithNone(extras.SelectDateWidget):
 
     def create_select(self, name, field, value, val, choices):
-        if self.required and val:  # reversed original statement
+        if self.is_required and val:  # reversed original statement
             choices.insert(0, self.none_value)
         return super(SelectDateWidgetWithNone, self) \
             .create_select(name, field, value, val, choices)
