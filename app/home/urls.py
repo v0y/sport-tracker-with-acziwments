2ba25bcd0c2e61bf431be34348551af5b05be77f
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-12-27 21:27:22 +0100

    fix home urls

--- app/home/urls.py
+++ app/home/urls.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 
-from django.conf.urls import patterns, url
+from django.conf.urls import (
+    patterns,
+    url,
+)
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('home.views',
+urlpatterns = patterns('app.home.views',
     url(r'^$',
         TemplateView.as_view(template_name='home/homepage.html'),
-        name="home"
-    )
+        name="home")
 )
