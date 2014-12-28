cd25ed6e44a609db705c6307bc684c5d32ac1a24
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-12-28 18:15:40 +0100

    refactor routes views structure

--- app/routes/urls.py
+++ app/routes/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('app.routes.api_views',
+urlpatterns = patterns('app.routes.views.api',
     url(r'^/api/upload_gpx$', 'upload_gpx', name='upload_gpx'),
     url(r'^/api/save_route$', 'save_route', name='save_route'),
     url(r'^/api/get_route_json$', 'get_route_json', name='get_route_json'),
